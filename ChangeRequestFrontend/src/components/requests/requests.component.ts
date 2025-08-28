7d04c95008eb742b6ad0d400726fb90598a3eef3
Alex Diaz <imloadinq7579@gmail.com>
2025-08-27 21:19:44 -0500

    user info display on request page

--- ChangeRequestFrontend/src/components/requests/requests.component.ts
+++ ChangeRequestFrontend/src/components/requests/requests.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ChangeRequestService, ChangeRequest } from '../../services/change-request/change-request.service';
 import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-requests',
@@ -10,13 +11,32 @@ import { CommonModule } from '@angular/common';
 })
 export class RequestsComponent implements OnInit {
   private changeRequestService = inject(ChangeRequestService);
+  private userService = inject(UserService);
   requests: ChangeRequest[] = [];
+  userName: string | null = null;
+  userType: string | null = null;
 
   ngOnInit(): void {
+    if (typeof window !== 'undefined') {
       this.loadRequests();
+      this.loadUserName();
+      this.loadUserType();
+    }
   }
 
   loadRequests() {
-    this.changeRequestService.getAllRequests().subscribe(data => this.requests = data);
+    this.changeRequestService.getAllRequests().subscribe({
+      next: data => this.requests = data,
+      error: err => console.error('Failed to load requests', err)
+    });
+  }
+
+  // maybe consolidate to one loadUserInfo()..
+  loadUserName() {
+    this.userName = this.userService.getUserName();
+  }
+
+  loadUserType() {
+    this.userType = this.userService.getUserType();
   }
 }
